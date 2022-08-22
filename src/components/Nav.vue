6108ee6a76bc43b7b0da3bfbf3705d1f4d9634f3
Yin <15297927693@163.com>
2022-08-23 00:28:07 +0800

    更换icon和view port

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,19 @@
 <template>
     <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
     <Icon name="label"/>
     标签
     </router-link>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
     <Icon name="money"/>
     记一笔</router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
     <Icon name="statistics"/>
     统计</router-link>
     </nav>
 </template>
 
 <script lang="ts">
-    let importAll =(requireContext:__WebpackModuleApi.RequireContext)=>requireContext.keys().forEach(requireContext);
-    try {
-    importAll(require.context('..assets/icons',true,/\.svg$/));
-    }catch(error){
-        console.log(error)
-    }
     export default {
         name:'Nav'
     }
@@ -29,6 +23,7 @@
     nav{
         display:flex;
         box-shadow: 0 0 3px rgba(0,0,0,0.3);
+        flex-direction: row;
         font-size: 12px;
         > .item{
             padding:2px 0;
@@ -41,6 +36,9 @@
                 width:32px;
                 height: 32px;
             }
+        }
+        > .item.selected{
+            color: orange;
         }
     }
 </style>
