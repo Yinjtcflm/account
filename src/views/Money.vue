6bd5f1c5c039b23c22beeba921d8cf0673e98c0f
Yin <15297927693@163.com>
2022-08-26 18:52:14 +0800

    可以选中或取消tag

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -4,11 +4,11 @@ import Layout from '@/components/Layout.vue';
     <NumberPad />
     <Types />
     <Notes />
-    <Tags />
+    <Tags :dataSource="tags" />
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Tags from "@/components/Money/Tags.vue";
@@ -17,6 +17,11 @@ import Notes from "@/components/Money/Notes.vue";
 export default {
   name: "Money",
   components: { NumberPad, Types, Tags, Notes },
+  data() {
+    return {
+      tags: ["衣", "食", "住", "行"],
+    };
+  },
 };
 </script>
 
