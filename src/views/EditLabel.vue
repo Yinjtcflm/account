b58a97941044f5f230b68ec841dc84582d79727e
Yin <15297927693@163.com>
2022-08-31 22:44:49 +0800

    展示tag的name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="notes-wrapper">
-      <Notes fieldName="标签名" placeholder="请输入标签名" />
+      <Notes :value="tag.name" fieldName="标签名" placeholder="请输入标签名" />
     </div>
     <div class="button-wrapper">
       <Button>删除标签</Button>
@@ -21,13 +21,14 @@ import { Component } from "vue-property-decorator";
 
 @Component
 export default class EditLabel extends Vue {
+  tag?: { id: string; name: string } = undefined;
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter((t) => t.id === id)[0];
     if (tag) {
-      console.log(tag);
+      this.tag = tag;
     } else {
       this.$router.replace("/404");
     }
