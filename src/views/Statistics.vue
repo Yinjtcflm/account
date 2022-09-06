da02508d3e6171c763ef55cd9e2a243238693b1c
Yin <15297927693@163.com>
2022-09-06 23:38:54 +0800

    在money.vue使用tabs

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,9 +1,13 @@
 <template>
   <Layout>
-    <Tabs classPrefix="type" :dataSource="typeList" :value.sync="type" />
     <Tabs
-      classPrefix="interval"
-      :dataSource="intervalList"
+      class-prefix="type"
+      :data-source="recordTypeList"
+      :value.sync="type"
+    />
+    <Tabs
+      class-prefix="interval"
+      :data-source="intervalList"
       :value.sync="interval"
     />
   </Layout>
@@ -26,20 +30,15 @@ import Types from "../components/Money/Types.vue";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
+import intervalList from "@/constants/intervalList";
+import recordTypeList from "@/constants/recordTypeList";
 @Component({
   components: { Tabs, Types },
 })
 export default class Statistics extends Vue {
   type = "-";
   interval = "day";
-  typeList = [
-    { text: "支出", value: "-" },
-    { text: "收入", value: "+" },
-  ];
-  intervalList = [
-    { text: "按天", value: "day" },
-    { text: "按月", value: "month" },
-    { text: "按年", value: "year" },
-  ];
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
 }
 </script>
