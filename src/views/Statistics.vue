833721c440461e99b84cd5470bf99c32fd1c2642
Yin <15297927693@163.com>
2022-09-13 18:50:24 +0800

    完成统计页面，修复页面进来commit的bug，统计页面备注显示bug等

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,22 +5,22 @@
       :data-source="recordTypeList"
       :value.sync="type"
     />
-    <div>
-      <ol>
-        <li v-for="(group, index) in groupedList" :key="index">
-          <h3 class="title">
-            {{ beautify(group.title) }}<span>￥{{ group.total }}</span>
-          </h3>
-          <ol>
-            <li v-for="item in group.items" :key="item.id" class="record">
-              <span>{{ tagString(item.tags) }}</span>
-              <span class="notes">{{ item.notes }}</span>
-              <span>￥{{ item.amount }}</span>
-            </li>
-          </ol>
-        </li>
-      </ol>
-    </div>
+
+    <ol v-if="groupedList.length > 0">
+      <li v-for="(group, index) in groupedList" :key="index">
+        <h3 class="title">
+          {{ beautify(group.title) }}<span>￥{{ group.total }}</span>
+        </h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
+    <div v-else class="noRecord">目前没有相关记录</div>
   </Layout>
 </template>
   
@@ -28,7 +28,6 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
-import intervalList from "@/constants/intervalList";
 import recordTypeList from "@/constants/recordTypeList";
 import dayjs from "dayjs";
 import clone from "@/lib/clone";
@@ -38,7 +37,7 @@ const oneDay = 86400 * 1000;
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
-    return tags.length === 0 ? "无" : tags.join(",");
+    return tags.length === 0 ? "无" : tags.map((t) => t.name).join("，");
   }
   beautify(string: string) {
     const day = dayjs(string);
@@ -60,14 +59,15 @@ export default class Statistics extends Vue {
   }
   get groupedList() {
     const { recordList } = this;
-    if (recordList.length === 0) {
-      return [];
-    }
+
     const newList = clone(recordList)
       .filter((r) => r.type === this.type)
       .sort(
         (a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf()
       );
+    if (newList.length === 0) {
+      return [];
+    }
     type Result = { title: string; total?: number; items: RecordItem[] }[];
     const result: Result = [
       {
@@ -102,6 +102,10 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
+.noRecord {
+  padding: 16px;
+  text-align: center;
+}
 ::v-deep {
   .type-tabs-item {
     background: #c4c4c4;
